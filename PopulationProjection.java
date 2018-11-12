6a67364ff66cbfda8f38a62e6a32f60b198781cb
Angelica Schell <Angelica@Angelicas-MacBook-Air.local>
2018-11-12 08:47:31 -0500

    I hope this works

--- PopulationProjection.java
+++ PopulationProjection.java
@@ -1,12 +1,10 @@
-package whatever;
-
 public class PopulationProjection {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int birth = 7*60*60*24*365;
-		int death = 3*60*60*24*365;
-		int immigrant = 45*60*60*24*365;
+		int birth = 1/7*60*60*24*365;
+		int death = 1/13*60*60*24*365;
+		int immigrant = 1/45*60*60*24*365;
 		int current_pop = 312032486;
 		for (int x = 0 ; x <= 4 ; x++) 
 			System.out.println((current_pop + birth - death + immigrant)*5);
